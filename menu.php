6f0dfb399dbfedac284a5eacfa4ec5da2a2381dc
nicolasverlhiac <nicolas.verlhiac@gmail.com>
2015-05-21 16:25:18 +0100

    ADD - MENU
    Faire un menu Stylisé en CSS

---
+++ menu.php
@@ -0,0 +1,14 @@
+<ul class="menu">
+	<li>
+		<a href="">Item 1</a>
+	</li>
+	<li>
+		<a href="">Item 2</a>
+	</li>
+	<li>
+		<a href="">Item 3</a>
+	</li>
+	<li>
+		<a href="">Item 4</a>
+	</li>
+</ul>
